d5a91d7f149879c55750e920a70857eab08ea7ae
lybu-4612 <101847235+lybu-4612@users.noreply.github.com>
2022-06-10 21:01:28 +0530

    Update views.py

--- gastyle1-h1/Broshop/views.py
+++ gastyle1-h1/Broshop/views.py
@@ -8,13 +8,19 @@ from store.models import Product,ReviewRating
 # Create your views here.
 
 def home(request):
-        
-        products = Product.objects.all().filter(is_available=True).order_by('-created_date')
-        for product in products:
-                reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        try:
+                products = Product.objects.all().filter(is_available=True).order_by('-created_date')
+         except:
+                return render(request, 'home.html')  
+#         for product in products:
+#                 try:
+#                       reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+#                 except:
+               
+                        
         context = {
                 'products': products,
-                'reviews': reviews,
+#  
         }
         
         return render(request, 'home.html', context)
